eea9b50bf1a0d6b48604d1a2253727cd9cd4cd3c
Adam Pegman <adam:pegman.space>
2018-12-19 18:38:58 +0000

    Edited a docstring.

--- amp_constraint_solver/__init__.py
+++ amp_constraint_solver/__init__.py
@@ -1,1 +1,22 @@
+"""
+# Constraint solving using domain reduction.
+
+eg. 
+```
+from amp_constraint_solver import solve 
+
+def constraint1(b, **variables):
+    return variables['a'] != 5 and b != 3
+
+constraint2 = lambda a, b: a != b
+
+for solution in solve({'a': [1, 5], 'b': [1, 2, 3]}, [constraint1, constraint2]):
+   print(solution)
+```
+
+* variables: Things that are assigned values in a possible solution.
+* values: Possible values for variables in a solution.
+* domains: Bags of possible valid values for a variable.
+* constraints: Conditions that are used to check if solutions are valid.
+"""
 from .constraint_solver import *
--- amp_constraint_solver/constraint_solver.py
+++ amp_constraint_solver/constraint_solver.py
@@ -14,14 +14,10 @@ for solution in solve({'a': [1, 5], 'b': [1, 2, 3]}, [constraint1, constraint2])
    print(solution)
 ```
 
-"""
-"""
-# Glossary:
-	* variables: Things that are assigned values in a possible solution.
-	* values: Possible values for variables in a solution.
-	* domains: Bags of possible valid values for a variable.
-	* constraints: Conditions that are used to check if solutions are valid.
-
+* variables: Things that are assigned values in a possible solution.
+* values: Possible values for variables in a solution.
+* domains: Bags of possible valid values for a variable.
+* constraints: Conditions that are used to check if solutions are valid.
 """
 from typing import cast as _cast, TypeVar, List, Dict, Set, Callable, Generator
 from inspect import getfullargspec as _getfullargspec
